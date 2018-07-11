ae902d3cf7ca81b6bead189ec54ec698255bf085
Venkat <Venkat@192.168.1.48>
2018-07-11 11:06:55 +0530

    ADDED PROFITMAKER PO

--- src/com/pdf/parser/ConvertCsvToExcel.java
+++ src/com/pdf/parser/ConvertCsvToExcel.java
@@ -11,6 +11,8 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.itextpdf.text.pdf.PdfReader;
+
 
 public class ConvertCsvToExcel {
 	public static final Logger _LOGGER = Logger.getLogger(ConvertCsvToExcel.class);
@@ -18,15 +20,31 @@ public class ConvertCsvToExcel {
 		
 	 //   String fileExtension = CommonUtility.getFileExtension(mfile.getOriginalFilename());
 	    File file = convertMultiPartFileIntoFile(mfile);
+	   // file.
 	    ZipSecureFile.setMinInflateRatio(0.001);
 	   /* ZipSecureFile.setMinInflateRatio(0.00000001);
 	    Long somev=4294967295L;
 	    ZipSecureFile.setMaxEntrySize(somev);*/
 	   String fileName =  mfile.getOriginalFilename();
+	  /* try {
+		PdfReader pdfFile = new PdfReader(new FileInputStream(file));
+		System.out.println("Pdf No.Of Pages:"+pdfFile.getNumberOfPages());
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}*/
 	   Workbook workBook = null;
-	   if(fileName.contains("PurchaseOrder")){
-		   try( FileInputStream inputStream = new FileInputStream("D:\\Sharing Files\\Users\\Amey\\pdf demo\\pdf excel parser demo\\2018-05-22PO.xlsx");
-				//FileInputStream inputStream = new FileInputStream("C:\\Users\\Venkat\\Documents\\pdf excel parser demo\\2018-05-22PO.xlsx");
+	   if(fileName.contains("Purchase Order_ProfitMaker") || fileName.equalsIgnoreCase("ProfitMaker_po_ext_description.PDF")){//
+		   try( FileInputStream inputStream = new FileInputStream("C:\\Users\\Venkat\\Documents\\pdf excel parser demo\\PurchaseOrder ProfitMaker.xlsx");
+	    			Workbook workBook2 = new XSSFWorkbook(inputStream)) {
+	    		return workBook2;
+			} catch (IOException e) {
+				_LOGGER.error("unable to file convert into excelsheet"+e);
+			} catch(Exception e){
+				_LOGGER.error("unable to file convert into WB: "+e.getCause());
+			}
+	   } else if(fileName.contains("PurchaseOrder")){
+		   try( FileInputStream inputStream = new FileInputStream("C:\\Users\\Venkat\\Documents\\pdf excel parser demo\\2018-05-22PO.xlsx");
 	    			Workbook workBook2 = new XSSFWorkbook(inputStream)) {
 	    		return workBook2;
 			} catch (IOException e) {
@@ -35,8 +53,7 @@ public class ConvertCsvToExcel {
 				_LOGGER.error("unable to file convert into WB: "+e.getCause());
 			}
 	   } else if(fileName.equals("Order279056Invoice124150208343.pdf")){
-		   try( FileInputStream inputStream = new FileInputStream("D:\\Sharing Files\\Users\\Amey\\pdf demo\\pdf excel parser demo\\invoice_8343.xlsx");
-				//FileInputStream inputStream = new FileInputStream("C:\\Users\\Venkat\\Documents\\pdf excel parser demo\\invoice_8343.xlsx");
+		   try( FileInputStream inputStream = new FileInputStream("C:\\Users\\Venkat\\Documents\\pdf excel parser demo\\invoice_8343.xlsx");
 	    			Workbook workBook2 = new XSSFWorkbook(inputStream)) {
 	    		return workBook2;
 			} catch (IOException e) {
--- src/com/pdf/parser/FileUpload.java
+++ src/com/pdf/parser/FileUpload.java
@@ -1,20 +1,14 @@
 package com.pdf.parser;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.pdf.model.PurchaseOrder;
+import com.pdf.model.UploadBean;
 
 
 
@@ -31,18 +27,25 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class FileUpload {
 	private static Logger _LOGGER = Logger.getLogger(Class.class);
 	@RequestMapping(method = RequestMethod.GET)
-	public String welcomePage(Map<String, Object> model) {
+	public ModelAndView welcomePage(Map<String, Object> model) {
 		
-		return "upload";
+		return new ModelAndView("upload", "uploadBean", new UploadBean());
 		/*FtpLoginBean ftpLogin = new FtpLoginBean(); //uncommented code while using ftp
 		model.put("ftpLoginBean", ftpLogin);
 		return "ftpLogin";*/ 
 		}
 	@RequestMapping(value="/parseFile",method=RequestMethod.POST)  
-	public ModelAndView upload(@RequestParam CommonsMultipartFile file,HttpSession session){ 
+	public void upload(@ModelAttribute("uploadBean") UploadBean uploadBean){
+		MultipartFile file =  uploadBean.getFile();
+		System.out.println(file.getOriginalFilename());
+		System.out.println(uploadBean.getFileType());
+	}
+	@RequestMapping(value="/parseFile123",method=RequestMethod.POST)  
+	public ModelAndView upload1(@RequestParam CommonsMultipartFile file,HttpSession session){ 
 		String filename="";
 		LinkedHashMap<String,String> valueMap=new LinkedHashMap<String, String>();
 		Mapclas contactForm = null;
+		List<PurchaseOrder> purchaseOrderList = null;
 		try{  
 	        String path=session.getServletContext().getRealPath("/");  
 	        filename=file.getOriginalFilename();  
@@ -51,7 +54,9 @@ public class FileUpload {
 	        Workbook workbook=  ConvertCsvToExcel.getWorkBook(file);
 	        if(filename.trim().equals("Order279056Invoice124150208343.pdf")){
 	        	valueMap=ExcelMapping.readExcel(workbook);	
-	        }else{
+	        } else if(filename.equalsIgnoreCase("Purchase Order_ProfitMaker.pdf") || filename.equalsIgnoreCase("ProfitMaker_po_ext_description.PDF")){
+	        	purchaseOrderList = ProfitMakerPoMapper.readExcel(workbook);
+	        } else{
 	        	valueMap=PurOrdParser.readExcel(workbook);
 	        }
 	        
@@ -72,7 +77,12 @@ public class FileUpload {
 	        }catch(Exception e){
 	        	System.out.println(e);
 	        }  
-	        return new ModelAndView("user","contactForm",contactForm);  
+		if(filename.contains("ProfitMaker")){
+			return new ModelAndView("profitMakerData","profitMakerPODataList",purchaseOrderList);
+		} else {
+			return new ModelAndView("user","contactForm",contactForm);
+		}
+	          
 	    }  
 }
 
